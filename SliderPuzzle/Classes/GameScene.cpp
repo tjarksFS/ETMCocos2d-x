f10f4907678f26cc21ad263f06f0086458f73ad9
Matthew Tjarks <tjarks@fullsail.edu>
2014-03-25 12:14:38 -0400

    Hello world, began using sprites

---
+++ SliderPuzzle/Classes/GameScene.cpp
@@ -0,0 +1,102 @@
+#include "GameScene.h"
+
+USING_NS_CC;
+
+Scene* GameScene::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = GameScene::create();
+    
+    // add layer as a child to scene
+    scene->addChild(layer);
+    
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool GameScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Point origin = Director::getInstance()->getVisibleOrigin();
+    
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
+    
+    // add a "close" icon to exit the progress. it's an autorelease object
+    auto closeItem = MenuItemImage::create(
+                                           "CloseNormal.png",
+                                           "CloseSelected.png",
+                                           CC_CALLBACK_1(GameScene::menuCloseCallback, this));
+    
+	closeItem->setPosition(Point(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
+                                 origin.y + closeItem->getContentSize().height/2));
+    
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(closeItem, NULL);
+    menu->setPosition(Point::ZERO);
+    this->addChild(menu, 1);
+    
+    /////////////////////////////
+    // 3. add your codes below...
+    
+    // add a label shows "Hello World"
+    // create and initialize a label
+    
+    auto label = LabelTTF::create("Game Scene", "Arial", 24);
+    
+    // position the label on the center of the screen
+    label->setPosition(Point(origin.x + visibleSize.width/2,
+                             origin.y + visibleSize.height - label->getContentSize().height));
+    
+    // add the label as a child to this layer
+    this->addChild(label, 1);
+    
+    // add "HelloWorld" splash screen"
+    auto sprite = Sprite::create("numberpicture.png");
+    
+    // position the sprite on the center of the screen
+    sprite->setPosition(Point(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+
+    sprite->setScale(visibleSize.height / sprite->getBoundingBox().size.height);
+    // add the sprite as a child to this layer
+    this->addChild(sprite, 0);
+    
+    
+    auto homeButton = MenuItemImage::create("homebutton.png", "homebutton.png",
+                                            CC_CALLBACK_1(GameScene::homeButtonCallback, this));
+    
+    homeButton->setPosition(300, 300);
+    auto homeButtonMenu = Menu::create(homeButton, nullptr);
+    homeButtonMenu->setPosition(Point::ZERO);
+    this->addChild(homeButtonMenu, 1);
+    
+    
+    
+    return true;
+}
+
+
+void GameScene::homeButtonCallback(Ref* pSender) {
+    Director::getInstance()->popScene();
+}
+
+void GameScene::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+    
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+}
